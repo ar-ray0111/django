38e21fe35fd0c73401a85602e2038c2a57cfc713
ar-ray0111 <arjunomray@gmail.com>
2023-07-14 04:08:00 +0530

    changed event name max limit

--- event/models.py
+++ event/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    event_name = models.CharField(max_length=20)
+    event_name = models.CharField(max_length=30)
     poster = models.URLField()
     registration_link = models.URLField(max_length=100)
     time = models.DateField()
